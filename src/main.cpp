c36c5c703e2a3baaad4dff9248060fca363cc54a
Kussauer <internet@nickkussauer.de>
2023-08-06 18:17:22 +0200

    Buttons über Analogpin + Tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,50 @@
 #include <Arduino.h>
 
 
+// === Taster ===
+int taster;
+int analogWert = 0;
+int value = 0;
 
-void setup() {
- 
+int tasterAbfrage()
+{
+  analogWert = analogRead(A5);
+  return analogWert;
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+}
+
+void loop()
+{
+  value = tasterAbfrage();
+
+  if ((value <= 4065) && (value >= 4059))
+  {
+    Serial.println("Taster 1");
+  }
+
+  if ((value <= 4039) && (value >= 4035))
+  {
+    Serial.println("Taster 2");
+  }
+
+  if ((value <= 3925) && (value >= 3915))
+  {
+    Serial.println("Taster 3");
+  }
+
+  if ((value <= 3750) && (value >= 3740))
+  {
+    Serial.println("Taster 4");
+  }
+
+  Serial.println(value);
+  delay(250);
+
 }
