f16e0ad96bc1f52b86b59494e70ad56c45743c7d
Kussauer <internet@nickkussauer.de>
2023-08-08 19:06:27 +0200

    Ausgabe Buttons ausgelagert, Millis gesetzt

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,14 @@
 #include <Arduino.h>
-/* #include "SSD1306Ascii.h"
-#include "SSD1306AsciiWire.h" */
-#include "button.h"
-#include "oledDisplay.h"
-
-// === OLED ===
-/* #define I2C_ADDRESS 0x3C // Adresse
-SSD1306AsciiWire monitorTransmitter; // Displayname  */
-
-// === Taster ===
-
-button taster1(8, 1, "Taster 1");
-button taster2(9, 2, "taster 2");
-button taster3(10, 3, "Taster 3");
-button taster4(11, 4, "Taster 4");
-button taster5(12, 5, "Taster 5");
-
+#include "outputs.h"
 
 
 // === StatusLED ===
 int LEDButton1 = 3;
 int statusLEDButton1 = LOW;
+
+// === MILLIS ===
+unsigned long previousMillisButtons = 0;
+const long intervallButtons = 50;
 
 void setup()
 {
@@ -33,36 +21,18 @@ void setup()
   digitalWrite(LEDButton1, statusLEDButton1);
 
   // === OLED ===
- /*  monitorTransmitter.begin(&Adafruit128x64, I2C_ADDRESS); // OLED 0,96
-  monitorTransmitter.setFont(Arial_bold_14);
-  monitorTransmitter.displayRemap(true);              // Drehung 180 Grad
-  monitorTransmitter.setScrollMode(SCROLL_MODE_AUTO); // AutoScroll
-  monitorTransmitter.setLetterSpacing(2);
-  monitorTransmitter.println("Systemstart"); */
-
   monitorInitial();
 
   // === Taster ===
-  
+
 }
 
 void loop()
 {
-
-if (taster1.readButton() == 1)
-{
- taster1.toggleButtonStatus();
- taster1.buttonSerialAusgabe();
- monitorTransmitter.clear();
- monitorTransmitter.println(taster1.buttonName);
- if (taster1.getButtonStatus() == 1){
-  monitorTransmitter.println("aktiviert");
- }
- if (taster1.getButtonStatus() == 0) {
-  monitorTransmitter.println("deaktiviert");
- }
- Serial.println(taster1.getButtonStatus());
-}
-
-
+  unsigned long currentMillis = millis();
+  if ((currentMillis - previousMillisButtons) >= intervallButtons)
+  {
+  checkTasterAll();
+  previousMillisButtons = currentMillis;
+  }
 }
---
+++ src/outputs.h
@@ -0,0 +1,119 @@
+#include "button.h"
+#include "oledDisplay.h"
+
+// === Initialisierung der Taster (PIN / Nummer / Name)
+button taster1(8, 1, "Taster 1");
+button taster2(9, 2, "Taster 2");
+button taster3(10, 3, "Taster 3");
+button taster4(11, 4, "Taster 4");
+button taster5(12, 5, "Taster 5");
+
+void checkTaster1()
+{
+    if (taster1.readButton() == 1)
+    {
+        taster1.toggleButtonStatus();
+        taster1.buttonSerialAusgabe();
+        monitorTransmitter.clear();
+        monitorTransmitter.println(taster1.buttonName);
+        if (taster1.getButtonStatus() == 1)
+        {
+            monitorTransmitter.println("aktiviert");
+        }
+        if (taster1.getButtonStatus() == 0)
+        {
+            monitorTransmitter.println("deaktiviert");
+        }
+        Serial.println(taster1.getButtonStatus());
+    }
+}
+
+void checkTaster2()
+{
+    if (taster2.readButton() == 1)
+    {
+        taster2.toggleButtonStatus();
+        taster2.buttonSerialAusgabe();
+        monitorTransmitter.clear();
+        monitorTransmitter.println(taster2.buttonName);
+        if (taster2.getButtonStatus() == 1)
+        {
+            monitorTransmitter.println("aktiviert");
+        }
+        if (taster2.getButtonStatus() == 0)
+        {
+            monitorTransmitter.println("deaktiviert");
+        }
+        Serial.println(taster2.getButtonStatus());
+    }
+}
+
+void checkTaster3()
+{
+    if (taster3.readButton() == 1)
+    {
+        taster3.toggleButtonStatus();
+        taster3.buttonSerialAusgabe();
+        monitorTransmitter.clear();
+        monitorTransmitter.println(taster3.buttonName);
+        if (taster3.getButtonStatus() == 1)
+        {
+            monitorTransmitter.println("aktiviert");
+        }
+        if (taster3.getButtonStatus() == 0)
+        {
+            monitorTransmitter.println("deaktiviert");
+        }
+        Serial.println(taster3.getButtonStatus());
+    }
+}
+
+void checkTaster4()
+{
+    if (taster4.readButton() == 1)
+    {
+        taster4.toggleButtonStatus();
+        taster4.buttonSerialAusgabe();
+        monitorTransmitter.clear();
+        monitorTransmitter.println(taster4.buttonName);
+        if (taster4.getButtonStatus() == 1)
+        {
+            monitorTransmitter.println("aktiviert");
+        }
+        if (taster4.getButtonStatus() == 0)
+        {
+            monitorTransmitter.println("deaktiviert");
+        }
+        Serial.println(taster4.getButtonStatus());
+    }
+}
+
+void checkTaster5()
+{
+    if (taster5.readButton() == 1)
+    {
+        taster5.toggleButtonStatus();
+        taster5.buttonSerialAusgabe();
+        monitorTransmitter.clear();
+        monitorTransmitter.println(taster5.buttonName);
+        if (taster5.getButtonStatus() == 1)
+        {
+            monitorTransmitter.println("aktiviert");
+        }
+        if (taster5.getButtonStatus() == 0)
+        {
+            monitorTransmitter.println("deaktiviert");
+        }
+        Serial.println(taster5.getButtonStatus());
+    }
+}
+
+void checkTasterAll() 
+{
+  checkTaster1();
+  checkTaster2();
+  checkTaster3();
+  checkTaster4();
+  checkTaster5();
+
+}
